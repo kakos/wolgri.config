c1a1734ec54fbfa3798a5d8d4b3ee3d6ce507c88
Volodymer Grigorenko <wolgri@gmail.com>
2008-10-14 19:24:50 +0300

    up

---
+++ sysycfg/wlan_create.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+/sbin/ifdown wlan0
+/sbin/ifdown wlan1
+/sbin/ifdown wlan2
+/usr/local/bin/madwifi-unload
+/sbin/modprobe ath_pci autocreate=none ath_debug=0 ieee80211_debug=0 outdoor=1
+/sbin/wlanconfig wlan1 create wlandev wifi0 wlanmode ap   # wlan1 ap 
+/sbin/wlanconfig wlan0 create wlandev wifi0 wlanmode sta # wlan0 managed 
+/sbin/wlanconfig wlan2 create wlandev wifi0 wlanmode monitor   # wlan2 monitor 
